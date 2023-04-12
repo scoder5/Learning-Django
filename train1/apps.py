e10831fe7648f56e2a1eb444b8b92a93217c0e5a
scoder5 <scoding83@gmail.com>
2023-04-12 12:13:27 +0530

    new commit

--- train1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Train1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'train1'
--- train1/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('login', views.login, name='login'),
-]
