d8d21f4c64f5262ff1de288ed488dec3c82d39a5
scoder5 <scoding83@gmail.com>
2023-04-12 15:54:41 +0530

    datetime display

--- Form/authentication/views.py
+++ Form/authentication/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    now = datetime.now()
+    data = {
+        'now': now,
+    }
+    return render(request, 'home.html', data)
 
 def signup(request):
 
